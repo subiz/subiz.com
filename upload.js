6dbf232389df967b04c58794a67e0ce4f2ec1d91
Thanh Pham Kieu <thanhpk@live.com>
2024-10-02 14:02:16 +0700

    add block

---
+++ upload.js
@@ -0,0 +1,23 @@
+var request = require('request')
+
+const testFolder = './block/'
+const fs = require('fs')
+
+fs.readdir(testFolder, (err, files) => {
+	files.forEach(async (file) => {
+		if (file == 'index') return
+		let data = fs.readFileSync(testFolder + file, {encoding: 'utf8'})
+		request(
+			{
+				url: 'https://api.subiz.com.vn/4.0/accounts/acpxkgumifuoofoosble/articles/-?x-access-token=v6ag.agqmwfyuehpuzpehmv.rtscrylnjxushqrqhddbyngambfcgsnyzugjefpajomfs&account-id=acpxkgumifuoofoosble',
+				method: 'POST',
+				body: data,
+			},
+			function (error, response, body) {
+				let out = JSON.parse(body)
+				if (out && out.article && out.article.id) return
+				console.log(file, body, error)
+			},
+		)
+	})
+})
