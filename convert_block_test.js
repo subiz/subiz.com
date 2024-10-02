f0987bf7db3897af65442c5a97a72d4245a725aa
Thanh Pham Kieu <thanhpk@live.com>
2024-10-02 12:36:16 +0700

    add convert block

---
+++ convert_block_test.js
@@ -0,0 +1,12 @@
+var html2Block = require('./convert_block.js')
+const fs = require('fs')
+
+const rawhtml = fs.readFileSync('./full.html', 'utf8')
+
+async function main() {
+	let docM = {}
+
+	let out = await html2Block(rawhtml, docM)
+		console.log('OUT', JSON.stringify(out))
+}
+main()
