e0b6a1b8a1eea45ea37ff9bc645abb5bdefde771
Duc Tran <ducck92@gmail.com>
2025-10-31 11:25:30 +0700

    fix embed video fullwidth and responsive

--- src/components/EmbedVideo.js
+++ src/components/EmbedVideo.js
@@ -2,12 +2,12 @@ import React from 'react'
 
 export default function EmbedVideo({src}) {
 	return (
-		<iframe
-			src={src}
-			style={{border: 'none'}}
-			height='360'
-			width='640'
-			allow='accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture;'
-			allowfullscreen></iframe>
+		<div style={{position: 'relative', width: '100%', paddingTop: '50%' /*16 : 9*/}}>
+			<iframe
+				src={src}
+				style={{border: 'none', position: 'absolute', inset: 0, width: '100%', height: '100%'}}
+				allow='accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture;'
+				allowfullscreen></iframe>
+		</div>
 	)
 }
