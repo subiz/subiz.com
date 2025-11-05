a2f89f78c91e78b0724a4dc7b9d67caa0a9007a1
Duc Tran <ducck92@gmail.com>
2025-11-05 09:22:04 +0700

    improve embed video resolution

--- src/components/EmbedVideo.js
+++ src/components/EmbedVideo.js
@@ -1,13 +1,31 @@
 import React from 'react'
 
-export default function EmbedVideo({src}) {
+export default function EmbedVideo({src, resolution = {}}) {
+	const MIN_HW_RATIO = 0.5 // for common video on youtube with resolution 1920x1080
+	const MAX_HW_RATIO = 2.2 // for common video on short with resolution 864x1920
+
+	let ratio = MIN_HW_RATIO
+	if (
+		resolution.width &&
+		resolution.height &&
+		typeof resolution.width === 'number' &&
+		typeof resolution.height === 'number'
+	) {
+		ratio = resolution.height / resolution.width
+		if (ratio > MAX_HW_RATIO) ratio = MAX_HW_RATIO
+		if (ratio < MIN_HW_RATIO) ratio = MIN_HW_RATIO
+	}
 	return (
-		<div style={{position: 'relative', width: '100%', paddingTop: '50%' /*16 : 9*/}}>
+		<div style={{position: 'relative', width: '100%', paddingTop: displayPercentage(ratio) /*16 : 9*/}}>
 			<iframe
 				src={src}
 				style={{border: 'none', position: 'absolute', inset: 0, width: '100%', height: '100%'}}
-				allow='accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture;'
-				allowfullscreen></iframe>
+				allow='accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture; fullscreen'
+				allowFullScreen></iframe>
 		</div>
 	)
+}
+
+function displayPercentage(ratio) {
+	return Math.round(ratio * 10_000) / 100 + '%'
 }
