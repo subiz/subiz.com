1eb7129614776ab5fffbf60ff4d6f7a2edad2762
Thanh Pham Kieu <thanhpk@live.com>
2025-12-24 17:48:23 +0700

    update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,12 +24,113 @@ const config = {
 			{
 				redirects: [
 					{
-						to: '/735781858-faqs-cau-hoi-thuong-gap-ve-zns',
+						to: '/352944605-bang-tinh-nang',
+						from: ['/docs/352944605-bang-tinh-nang-subiz'],
+					},
+					{
+						to: '/1543153800-mau-tin-nhan-zns',
+						from: ['/1543153800-cach-tao-mau-tin-nhan-zns'],
+					},
+					{
+						to: '/300561377-gui-tin-nhan-zns',
+						from: ['/300561377-gui-tin-nhan-zns-tren-subiz'],
+					},
+					{
+						to: '/735781858-faqs-ve-zns',
+						from: ['/735781858-faqs-cau-hoi-thuong-gap-ve-zns'],
+					},
+					{
+						to: '/1168493719-email-marketing',
+						from: ['/1168493719-gui-hang-loat-email-marketing'],
+					},
+					{
+						to: '/583253150-cua-so-chat',
+						from: ['/583253150-chinh-sua-cua-so-chat-dep'],
+					},
+					{
+						to: '/1139647591-fanpage',
+						from: ['/1139647591-ket-noi-fanpage'],
+					},
+					{
+						to: '/1068952124-tu-dong-phan-hoi-fanpage',
+						from: ['/1068952124-cac-tinh-nang-subiz-ho-tro-fanpage'],
+					},
+					{
+						to: '/1579085486-zalo-ca-nhan',
+						from: ['/1579085486-tich-hop-zalo-ca-nhan-vao-subiz'],
+					},
+					{
+						to: '/297964546-zalo-oa',
+						from: ['/297964546-ket-noi-zalo-oa'],
+					},
+					{
+						to: '/1245788391-email',
+						from: ['/1245788391-ket-noi-email'],
+					},
+					{
+						to: '/984010358-google-business',
+						from: ['/984010358-kenh-google'],
+					},
+					{
+						to: '/732776091-instagram',
+						from: ['/732776091-ket-noi-instagram'],
+					},
+					{
+						to: '/628554948-agent',
+						from: ['/628554948-agent-la-gi'],
+					},
+					{
+						to: '/972963943-rule-phan-phoi',
+						from: ['/972963943-rule-phan-phoi-hoi-thoai'],
+					},
+					{
+						to: '/33314021-ban-giao-truoc-khi-agent-nghi',
+						from: ['/33314021-xu-ly-khi-co-nhan-vien-nghi-viec-tren-subiz'],
+					},
+					{
+						to: '/1540866648-auto-chatbot',
+						from: ['/1540866648-bot-tu-dong-nhan-tin'],
+					},
+					{
+						to: '/102007810-kich-ban-chatbot-mau',
+						from: ['/102007810-mot-so-mau-kich-ban-bot'],
+					},
+					{
+						to: '/698043010-chatbot-ai',
+						from: ['/698043010-su-dung-subiz-chatbot-ai'],
+					},
+					{
+						to: '/1083215603-popup',
+						from: ['/1083215603-popup-chuyen-doi-khach-tiem-nang'],
+					},
+					{
+						to: '/1968656234-subiz-live',
+						from: ['/1968656234-subiz-live-khach-truy-cap-web'],
+					},
+					{
+						to: '/1221805713-khach-hang-tiem-nang',
+						from: ['/1221805713-quan-ly-khach-tiem-nang'],
+					},
+					{
+						to: '/1802811302-nhan-phan-loai',
+						from: ['/1802811302-nhan-phan-loai-khach-hang'],
+					},
+					{
+						to: '/456469809-tich-hop-dau-so',
+						from: ['/456469809-ket-noi-so-tong-dai'],
+					},
+
+					{
+						to: '/735781858-faqs-ve-zns',
 						from: ['/docs/735781858-faqs---cau-hoi-thuong-gap-ve-zns'],
 					},
 					{
-						to: '/735781858-faqs-cau-hoi-thuong-gap-ve-zns',
+						to: '/735781858-faqs-ve-zns',
 						from: ['/735781858-faqs---cau-hoi-thuong-gap-ve-zns'],
+					},
+					{
+						to: '/735781858-faqs-ve-zns',
+						from: ['/735781858-faqs-cau-hoi-thuong-gap-ve-zns'],
 					},
 				],
 			},
